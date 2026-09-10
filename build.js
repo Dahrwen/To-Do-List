9b93e37dff251f1d7fefbfaba8bd2c8e152e50ce
Dahrwen <dharwindecembrada@gmail.com>
2026-09-10 20:19:10 +0800

    build.js ulit

--- build.js
+++ build.js
@@ -4,8 +4,9 @@ const fs = require('fs');
 const supabaseUrl = process.env.SUPABASE_URL || '';
 const supabaseKey = process.env.SUPABASE_KEY || '';
 
-const content = const SUPABASE_URL = "${supabaseUrl}"; \nconst
-SUPABASE_KEY = "${supabaseKey}"; \n;
+const content = `const SUPABASE_URL = "${supabaseUrl}";
+const SUPABASE_KEY = "${supabaseKey}";
+`;
 
 fs.writeFileSync('config.js', content);
 console.log('config.js generated successfully for deployment.');
